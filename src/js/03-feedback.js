525056df180cdab8210f48a23a82095d8a15f4b3
Roman Zeno <zeno385@gmail.com>
2022-01-10 15:30:26 +0200

    fix

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,40 +1,35 @@
 import throttle from 'lodash.throttle';
-import '../css/common.css';
-import '../css/03-feedback.css';
+// import '../css/common.css';
+// import '../css/03-feedback.css';
 
 const STORAGE_KEY = 'feedback-form-state';
 const formData = {};
 
-const refs = {
-  form: document.querySelector('.feedback-form'),
-  input: document.querySelector('input'),
-  textareaPas: document.querySelector('.feedback-form textarea'),
-};
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('input');
+const textarea = document.querySelector('textarea');
 
-refs.form.addEventListener('submit', onSubmitEmail);
-refs.textareaPas.addEventListener('input', throttle(onTextareaPas, 500));
-refs.form.addEventListener('input', e => {
-  formData[e.target.name] = e.target.value;
-  // localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-  console.log(formData);
-});
-
+const password = localStorage.getItem(STORAGE_KEY);
 popularTextareaPas();
 
-function onSubmitEmail(e) {
-  e.preventDefault();
-  e.target.reset();
-  localStorage.removeItem(STORAGE_KEY);
-}
-function onTextareaPas(e) {
-  const pass = e.target.value;
-  localStorage.setItem(STORAGE_KEY, pass);
- 
+form.addEventListener('input', throttle(onFormInput, 500));
+function onFormInput(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
 function popularTextareaPas() {
-  const savePass = localStorage.getItem(STORAGE_KEY);
+  const savePass = JSON.parse(password);
   if (savePass) {
-    refs.textareaPas.value = savePass;
+    // console.log(savePass);
+    input.value = savePass.email;
+    textarea.value = savePass.password;
   }
+}
+form.addEventListener('submit', onSubmitEmail);
+
+function onSubmitEmail(e) {
+  e.preventDefault();
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
 }
